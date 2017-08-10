68657f7f7294c85346b4e376d3e38159e30c136e
Bernat Ferragut <elbernat@gmail.com>
2017-08-10 17:44:56 -0400

    accessing firebase database

--- 2-game/main.js
+++ 2-game/main.js
@@ -5,42 +5,12 @@ window.addEventListener('load', function() {
 
     // Select Button
     const myButton = document.querySelector('.btn');
-
     // Select Score
     const result = document.querySelector('.result');
-
     // Select Input
     const player = document.querySelector('input');
-
     //Select Submit
     const submit = document.querySelector('.submit');
-
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyDJOIovuSj706mkKosauT_cfKndaHcyZ-U",
-        authDomain: "simple-db-1.firebaseapp.com",
-        databaseURL: "https://simple-db-1.firebaseio.com",
-        projectId: "simple-db-1",
-        storageBucket: "simple-db-1.appspot.com",
-        messagingSenderId: "335823253560"
-    };
-    firebase.initializeApp(config);
-
-     // db Connection
-    const database = firebase.database();
-
-    // Create data Object to Submit to database
-    submit.addEventListener( 'click', function() {
-        var data = {
-            name: player.value,
-            score: score
-        }
-        console.log(data);
-        // accessing the 'scores' node
-        const ref = database.ref('scores'); 
-        // adding Data to Database
-        ref.push(data);
-    });
 
     // Game
     let score = result.textContent;
@@ -51,6 +21,41 @@ window.addEventListener('load', function() {
          result.textContent =  score;
 
         return score && player.value;
+    });
+
+     // db Connection
+    const database = firebase.database();
+    // accessing the 'scores' node
+    const ref = database.ref('scores'); 
+    // start to bind different kind of events like data or a 'value'
+    ref.on('value', gotData, errData);
+    //Functions
+    function gotData(data) {
+       // console.log(data.val());
+       const keys = Object.keys(data.val());
+       console.log(keys);
+       keys.forEach(function(key) {
+           let name = key.name;
+           console.log(name);
+           let score = key.score;
+       })
+    }
+
+    function errData(err) {
+        console.log('Error!');
+        console.log(err);
+    }
+
+    // Create data Object to Submit to database
+    submit.addEventListener( 'click', function() {
+        var data = {
+            name: player.value,
+            score: score
+        }
+        // accessing the 'scores' node
+        ref = database.ref('scores'); 
+        // adding Data to Database
+        ref.push(data);
     });
 })
 
